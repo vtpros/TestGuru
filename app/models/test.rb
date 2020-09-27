0275f2585a5178e6e3f2569354907f46641d74ee
Vladimir Trachenko <trachenko@gmail.com>
2020-09-28 00:19:48 +0300

    Change tests_by_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
 
   def self.tests_by_category(name)
     category_id = Category.where(name: name).first.id
-    self.where(category_id: category_id).to_a
+    tests = Test.order(title: :desc).where(category_id: category_id).to_a
+    tests.map(&:title)
   end
 end
