92339c93437768d31888b1d7f993110fa196c8d6
Vladimir Trachenko <trachenko@gmail.com>
2020-09-29 11:41:53 +0300

    Fix final code review remarks

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,11 +2,9 @@ class Test < ApplicationRecord
   # belongs_to :category
 
   def self.tests_by_category(name)
-    # I don't know how to 'return array of test titles'
-    # without using .to_a and .map
-    Test.joins("JOIN categories ON tests.category_id = categories.id")
-    .where("categories.name = ?", name)
-    .order(title: :desc)
-    .to_a.map(&:title)
+    joins("JOIN categories ON tests.category_id = categories.id")
+      .where(categories: { name: name })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins("JOIN user_tests ON user_tests.test_id = tests.id")
-    .where("user_tests.user_id = ? AND tests.level = ?", self.id, level)    
+    Test
+      .joins("JOIN user_tests ON user_tests.test_id = tests.id")
+      .where(user_tests: {user_id: self.id}, tests: {level: level})
   end
 end
