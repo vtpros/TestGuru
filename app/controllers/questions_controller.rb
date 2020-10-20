74f99eb87d9befe704f7c7f4a75ba3831fb56f55
Vladimir Trachenko <trachenko@gmail.com>
2020-10-20 20:02:47 +0300

    Update app/controllers/questions_controller.rb
    Co-authored-by: max-underthesun <max.underthesun@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @questions = Question.where(test: @test)
+    @questions = @test.questions
   end
 
   def show
