b2b1ce80518c908434442e8e999cf84928aa8c61
Vladimir Trachenko <trachenko@gmail.com>
2020-11-01 01:44:04 +0300

    Fix copypaste error

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -36,7 +36,7 @@ class TestsController < ApplicationController
     if @test.destroy
       redirect_to action: :index
     else
-      render 'shared/errors/_errors_list', errors: @question.errors
+      render 'shared/errors/_errors_list', errors: @test.errors
     end
   end
 
