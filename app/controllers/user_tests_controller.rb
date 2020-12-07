33f37e84d4921017c165528885370a80ef9645c1
Vladimir Trachenko <trachenko@gmail.com>
2020-12-07 16:06:28 +0300

    Files for the third sreencast

--- app/controllers/user_tests_controller.rb
+++ app/controllers/user_tests_controller.rb
@@ -1,7 +1,28 @@
 class UserTestsController < ApplicationController
-  def show; end
+  before_action :set_user_test, only: %i[show update result]
+
+  def show
+
+  end
 
   def result; end
 
-  def update; end
+  def update
+    @user_test.accept!(params[:answer_ids])
+    if @user_test.completed?
+      redirect_to result_user_test_path(@user_test)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_user_test
+    @user_test = UserTest.find(params[:id])
+  end
+
+  def user_test_params
+    #params.require(:answer).permit(:answer_ids)
+  end
 end
--- app/models/user_test.rb
+++ app/models/user_test.rb
@@ -1,13 +1,44 @@
 class UserTest < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name:'Question', foreign_key: :current_question_id, optional: true
+  belongs_to :current_question, class_name:'Question',
+             foreign_key: :current_question_id, optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  #before_validation :before_validation_set_first_question, on: :create
+  before_save :before_save_set_next_question
+
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    #self.current_question = next_question
+    save!
+  end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  #def before_validation_set_first_question
+  #  self.current_question = test.questions.first if test.present?
+  #end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+    (correct_answers_count == answer_ids.count)
+  end
+
+  def before_save_set_next_question
+    current_id = current_question ? current_question.id : 0
+    next_question = test.questions.order(:id).where('id > :id', id: current_id).first
+    self.current_question = next_question
   end
 end
