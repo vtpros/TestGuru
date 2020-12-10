94aced5625675b5bf006ccdfd33fdc4a5736357f
Vladimir Trachenko <trachenko@gmail.com>
2020-12-10 16:10:09 +0300

    Files for the second screencast

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
-require 'digest/sha1'
+# require 'digest/sha1'
 
 class User < ApplicationRecord
-
-  include Auth
 
   has_many :created_tests, class_name:'Test', foreign_key: :author_id,
            dependent: :restrict_with_error
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  has_secure_password  
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
