30340bbcf590c7b77bbc7681f59bdc5271a5b8d6
Vladimir Trachenko <trachenko@gmail.com>
2020-12-15 15:13:50 +0300

    Fix todo rubocop warnings

--- db/migrate/20201201063754_add_currentquestion_to_usertests.rb
+++ db/migrate/20201201063754_add_currentquestion_to_usertests.rb
@@ -1,6 +1,7 @@
 class AddCurrentquestionToUsertests < ActiveRecord::Migration[6.0]
   def change
     # Creates a current_question_id column and a foreign key to the questions table
-    add_reference :user_tests, :current_question, foreign_key: {to_table: :questions}, default: nil
+    add_reference :user_tests, :current_question,
+                  foreign_key: { to_table: :questions }, default: nil
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,64 +7,64 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 categories = Category.create!(
-  [{name: 'Spanish'}, {name: 'Portuguese'}, {name: 'French'}]
+  [{ name: 'Spanish' }, { name: 'Portuguese' }, { name: 'French' }]
 )
 
 users = User.create!(
-  [{email: 'v@v.com', password: '123'}]
+  [{ email: 'v@v.com', password: '123' }]
 )
 
 tests = Test.create!(
   [
-    {title: 'Basic spanish', level: 1, category: categories[0], author: users[0]},
-    {title: 'Advanced spanish', level: 2, category: categories[0], author: users[0]},
-    {title: 'Basic portuguese', level: 1, category: categories[1], author: users[0]},
-    {title: 'Advanced portuguese', level: 5, category: categories[1], author: users[0]},
-    {title: 'Basic french', level: 3, category: categories[2], author: users[0]}
+    { title: 'Basic spanish', level: 1, category: categories[0], author: users[0] },
+    { title: 'Advanced spanish', level: 2, category: categories[0], author: users[0] },
+    { title: 'Basic portuguese', level: 1, category: categories[1], author: users[0] },
+    { title: 'Advanced portuguese', level: 5, category: categories[1], author: users[0] },
+    { title: 'Basic french', level: 3, category: categories[2], author: users[0] }
   ]
 )
 
 questions = Question.create!(
   [
-    {body: 'A dog', test: tests[0]},
-    {body: 'A cat', test: tests[0]},
-    {body: 'I have not seen him', test: tests[1]},
-    {body: 'I have just called you', test: tests[1]},
-    {body: 'A dog', test: tests[2]},
-    {body: 'A cat', test: tests[2]},
-    {body: 'I have not seen him', test: tests[3]},
-    {body: 'I have just called you', test: tests[3]}
+    { body: 'A dog', test: tests[0] },
+    { body: 'A cat', test: tests[0] },
+    { body: 'I have not seen him', test: tests[1] },
+    { body: 'I have just called you', test: tests[1] },
+    { body: 'A dog', test: tests[2] },
+    { body: 'A cat', test: tests[2] },
+    { body: 'I have not seen him', test: tests[3] },
+    { body: 'I have just called you', test: tests[3] }
   ]
 )
 
 answers = Answer.create!(
   [
-    {body: 'Un perro', question: questions[0], correct: true},
-    {body: 'Un gato', question: questions[0], correct: false},
-    {body: 'Una casa', question: questions[0], correct: false},
-    {body: 'Un cielo', question: questions[0], correct: false},
-    {body: 'Un perro', question: questions[1], correct: false},
-    {body: 'Un gato', question: questions[1], correct: true},
-    {body: 'No lo he visto', question: questions[2], correct: true},
-    {body: 'No lo vi', question: questions[2], correct: false},
-    {body: 'Te he llegado', question: questions[3], correct: true},
-    {body: 'Te llego', question: questions[3], correct: false},
-    {body: 'Um cão', question: questions[4], correct: true},
-    {body: 'Um gato', question: questions[4], correct: false},
-    {body: 'Um cão', question: questions[5], correct: false},
-    {body: 'Um gato', question: questions[5], correct: true},
-    {body: 'Não te vi', question: questions[6], correct: true},
-    {body: 'Não te oi', question: questions[6], correct: false},
-    {body: 'Acabei de te ligar', question: questions[7], correct: true},
-    {body: 'Te liguei', question: questions[7], correct: false}
+    { body: 'Un perro', question: questions[0], correct: true },
+    { body: 'Un gato', question: questions[0], correct: false },
+    { body: 'Una casa', question: questions[0], correct: false },
+    { body: 'Un cielo', question: questions[0], correct: false },
+    { body: 'Un perro', question: questions[1], correct: false },
+    { body: 'Un gato', question: questions[1], correct: true },
+    { body: 'No lo he visto', question: questions[2], correct: true },
+    { body: 'No lo vi', question: questions[2], correct: false },
+    { body: 'Te he llegado', question: questions[3], correct: true },
+    { body: 'Te llego', question: questions[3], correct: false },
+    { body: 'Um cão', question: questions[4], correct: true },
+    { body: 'Um gato', question: questions[4], correct: false },
+    { body: 'Um cão', question: questions[5], correct: false },
+    { body: 'Um gato', question: questions[5], correct: true },
+    { body: 'Não te vi', question: questions[6], correct: true },
+    { body: 'Não te oi', question: questions[6], correct: false },
+    { body: 'Acabei de te ligar', question: questions[7], correct: true },
+    { body: 'Te liguei', question: questions[7], correct: false }
   ]
 )
 
 test_passages = TestPassage.create!(
-  [{result: nil, completed: false, user: users[0], test: tests[0]}]
+  [{ result: nil, completed: false, user: users[0], test: tests[0] }]
 )
 
 # Should fail
-# answers1 = Answer.create!( [{body: 'Una agua', question: questions[0], correct: true}] )
-# tests1 = Test.create!( [ {title: 'Basic spanish', level: 1, category: categories[0], author: users[0]} ] )
-# tests1 = Test.create!( [ {title: 'Medium spanish', level: 0, category: categories[0], author: users[0]} ] )
+# answers1 = Answer.create!( [{ body: 'Una agua', question: questions[0], correct: true }] )
+# tests1 = Test.create!( [ { title: 'Basic spanish', level: 1, category: categories[0], author: users[0] } ] )
+# tests1 = Test.create!( [ { title: 'Medium spanish', level: 0, category: categories[0], author: users[0] } ] )
