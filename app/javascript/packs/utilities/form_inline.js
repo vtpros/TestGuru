36765892074f8a278b1628aa887b9b6b48291a16
Vladimir Trachenko <trachenko@gmail.com>
2021-03-16 16:37:26 +0300

    Finish task 1

--- app/javascript/packs/utilities/form_inline.js
+++ app/javascript/packs/utilities/form_inline.js
@@ -1,23 +1,16 @@
 function tdInlineHandler(testId) {
   let data = `[data-test-id="${testId}"]`
   let link = document.querySelector('.form-inline-link' + data)
-  // let testTitle = document.querySelector('.test-title' + data)
-  // let formInline = document.querySelector('.form-inline' + data)
   let $testTitle = $('.test-title' + data)
   let $formInline = $('.form-inline' + data)
 
   $testTitle.toggle()
   $formInline.toggle()
 
-  // if (formInline.classList.contains('hide')) {
   if ($formInline.is(':visible')) {
-    // testTitle.classList.add('hide')
-    // formInline.classList.remove('hide')
-    link.textContent = I18n.t('app.cancel') //'Cancel'
+    link.textContent = I18n.t('app.cancel');
   } else {
-    // testTitle.classList.remove('hide')
-    // formInline.classList.add('hide')
-    link.textContent = I18n.t('app.edit') //'Edit'
+    link.textContent = I18n.t('app.edit');
   }
 }
 
@@ -29,19 +22,13 @@ function formInlineHandler(event) {
 }
 
 document.addEventListener('turbolinks:load', function() {
-  // let controls = document.querySelectorAll('.form-inline-link')
-  //
-  // if (controls.length) {
-  //     controls[i].addEventListener('click', formInlineHandler)
-  //   }
-  // }
   I18n.locale = $('body').data('locale')
 
   $('.form-inline-link').on('click', formInlineHandler)
 
   let errors = document.querySelector('.resource-errors')
 
-  if (errors) {
+  if (errors && document.querySelector('.tests-index')) {
     let resourceId = errors.dataset.resourceId
     tdInlineHandler(resourceId)
   }
