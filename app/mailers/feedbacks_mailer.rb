938f07d4c3da5b173ecf37ec0a2be47a456fbc3b
Vladimir Trachenko <trachenko@gmail.com>
2021-03-25 09:40:20 +0300

    Refactor feedbacks mailer

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -2,7 +2,7 @@ class FeedbacksMailer < ApplicationMailer
   def feedback(text, user)
     @text = text
     @user = user
-    admin = User.where(type: 'Admin').first    
+    admin = Admin.first
 
     mail to: admin.email, subject: "New feedback"
   end
